4f8c45aaf43e7b2a34dc3455225c1d96868c6f4a
Boglarka Bodnar <bodnarbog94@gmail.com>
2023-03-07 09:48:40 +0100

    init map app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import { useState, useEffect } from 'react'
 
 function App() {
+
+  const [data, setData] = useState({})
+  const [europeData, seteuropeData] = useState({})
+
+  useEffect(() => {
+    async function getData() {
+      let res = await fetch('https://supercharge.info/service/supercharge/allSites')
+      let mapInfo = await res.json()
+      setData(mapInfo)
+    }
+    getData()
+  }, [])
+
+  useEffect(() => {
+    if (data.length > 0) {
+      const filterData = data.filter(element => element.address.region === "Europe")
+      seteuropeData(filterData)
+    }
+    console.log('lefut datauseeffect')
+  }, [data])
+
+  function apiBtn() {
+    console.log('europeData:', europeData)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={apiBtn}>API</button>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {europeData.map(tesla=>(
+            <Marker 
+            key={tesla.id}
+            position={[tesla.gps.latitude, tesla.gps.longitude]}>
+            </Marker>
+        ))}
+      </MapContainer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
