34c6ffd23486484f6eff8c2a429438116013c070
Boglarka Bodnar <bodnarbog94@gmail.com>
2023-03-07 13:57:42 +0100

    refactor region,center usestate

--- src/App.js
+++ src/App.js
@@ -4,7 +4,25 @@ import { useState, useEffect } from 'react'
 function App() {
 
   const [data, setData] = useState({})
-  const [europeData, seteuropeData] = useState({})
+  const [filteredData, setFilteredData] = useState({})
+  const [selectedRegion, setSelectedRegion] = useState({
+    region: 'Europe',
+    center: [48.227093, 7.753694]
+  })
+  const regions = [
+    {
+      "region": "Europe",
+      "center": [48.227093, 7.753694]
+    },
+    {
+      "region": "Asia Pacific",
+      "center": [33.60084662, 114.0130563]
+    },
+    {
+      "region": "North America",
+      "center": [41.220935, -95.835622]
+    }
+  ]
 
   useEffect(() => {
     async function getData() {
@@ -17,31 +35,38 @@ function App() {
 
   useEffect(() => {
     if (data.length > 0) {
-      const filterData = data.filter(element => element.address.region === "Europe")
-      seteuropeData(filterData)
+      const filterData = data.filter(element => element.address.region === selectedRegion.region)
+      setFilteredData(filterData)
     }
-  }, [data])
+  }, [data, selectedRegion.region])
 
 
+  function changeRegion(e) {
+    let newRegion = regions.filter(region => region.region === e.target.value)
+    setSelectedRegion({
+      region: newRegion[0].region,
+      center: newRegion[0].center
+    })
+  }
 
   return (
     <div className="App">
       <h1 className='page-title'>Tesla supercharger map</h1>
       <div className='select-region-container'>
         <p>Select region:</p>
-        <select>
+        <select onChange={(e) => { changeRegion(e) }} className='select-region-dropdown'>
           <option>Europe</option>
           <option>Asia Pacific</option>
           <option>North America</option>
         </select>
       </div>
-      <MapContainer center={[51.505, -0.09]} zoom={6} scrollWheelZoom={true}>
+      <MapContainer center={selectedRegion.center} zoom={6} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {europeData.length > 0 &&
-          europeData.map(tesla => (
+        {filteredData.length > 0 &&
+          filteredData.map(tesla => (
             <Marker
               key={tesla.id}
               position={[tesla.gps.latitude, tesla.gps.longitude]}>
